bf7296812e02b80372b1471fd9d3f82ada02e31f
Glenn Chon <8988140+GlennChon@users.noreply.github.com>
2022-12-30 12:36:51 -0500

    fix asset prefix path

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/profile/' : '',
+  assetPrefix: isProd ? '/profile' : '',
   images: {
     unoptimized: true,
   },
