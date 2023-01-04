5344f4b25c16bc2942eb4deef797f4eb003ef772
Glenn Chon <8988140+GlennChon@users.noreply.github.com>
2023-01-04 16:17:30 -0500

    recommit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import { Desk } from 'components/desk'
 import css from "../styles/Home.module.css"
 import { Canvas } from '@react-three/fiber'
@@ -10,9 +10,11 @@ import { degToRad } from 'three/src/math/MathUtils'
 
 export default function Home() {
   const groupMesh = useRef<Group>(null)
-
+  // TODO: Combine GLB models into 1 file
+  // Camera move to click locations
+  // https://codesandbox.io/s/bounds-and-makedefault-rz2g0
   // https://codesandbox.io/s/basic-clerping-example-qh8vhf?file=/src/Scene.js
-
+  // Route transitions https://codesandbox.io/s/router-transitions-4j2q2
   return (
     <div className={css.scene}>
       <Canvas
@@ -28,7 +30,6 @@ export default function Home() {
           <Keyboard
             groupPos={new Vector3(-8, 0.3, 0)}
             groupRot={new Euler(0, degToRad(-13), 0)} />
-          {/* TODO: xl MousePad, Mouse */}
           <Desk
             groupScale={new Vector3(0.8, 0.75, 0.75)}
             groupPos={new Vector3(0, -56.1, 0)}
